91aed077b568e7d13477aae1f4f8fc961283f020
israr2025 <mrisrar752@gmail.com>
2026-07-16 00:07:02 +0530

    add sum mehtod

--- src/demo/DemoFile.java
+++ src/demo/DemoFile.java
@@ -1,7 +1,18 @@
 package demo;
 
+import java.util.Scanner;
+
 public class DemoFile {
     public static void main(String[] args) {
+        Scanner cs=new Scanner(System.in);
+        System.out.println("Enter the Number1:");
+        int num1=cs.nextInt();
+        System.out.println("Enter the Number 2:");
+        int num2=cs.nextInt();
+
+        System.out.println("the to sum is "+num1+num2);
+
+
 
     }
 }
